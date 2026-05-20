b4f09264547c12161020634618fb056444300334
Arnab Dinda <arnabexpress3.12@gmail.com>
2026-05-20 15:39:43 +0530

    add trust proxy

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -25,6 +25,9 @@ export const signup = async (request, response, next) => {
     const user = await User.create({ email, password });
 
     const isProduction = process.env.NODE_ENV === "production";
+
+    app.set("trust proxy", 1);
+
     response.cookie("jwt", createToken(email, user.id), {
       maxAge,
       secure: isProduction,
@@ -65,6 +68,9 @@ export const login = async (request, response, next) => {
       return response.status(400).send("Invalid Email or Password");
     }
     const isProduction = process.env.NODE_ENV === "production";
+
+    app.set("trust proxy", 1);
+
     response.cookie("jwt", createToken(email, user.id), {
       maxAge,
       secure: isProduction,
